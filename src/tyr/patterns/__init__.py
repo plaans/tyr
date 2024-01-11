d495f6b1ec0874a369af0832c8e2528ae354ee32
Roland Godet <r.godet@outlook.fr>
2024-01-11 08:19:13 +0100

    feat(patterns): add AbstractSingleton metaclass

--- src/tyr/patterns/__init__.py
+++ src/tyr/patterns/__init__.py
@@ -1,2 +1,8 @@
+from . import abstract as a
+from . import singleton as s
 from .abstract import Abstract
 from .singleton import Singleton
+
+
+class AbstractSingletonMeta(a.AbstractMeta, s.SingletonMeta):
+    """An abstract thread-safe singleton which cannot be instantiated but its children can."""
---
+++ tests/tyr/patterns/abstract_singleton_test.py
@@ -0,0 +1,21 @@
+from tests.utils import assert_is_abstract, assert_is_not_abstract, assert_is_singleton
+from tyr import Abstract, AbstractSingletonMeta, Singleton
+
+
+class ChildA(Abstract, Singleton, metaclass=AbstractSingletonMeta):
+    pass
+
+
+class ChildB(ChildA):
+    pass
+
+
+class TestAbstractSingleton:
+    def test_child_instantiation_raises_error(self):
+        assert_is_abstract(ChildA)
+
+    def test_subchild_instantiation_is_ok(self):
+        assert_is_not_abstract(ChildB)
+
+    def test_is_singleton(self):
+        assert_is_singleton(ChildB)
