fe32cdafe177fad59d14d0302dbb8ae556e646c1
Roland Godet <r.godet@outlook.fr>
2024-01-12 16:37:07 +0100

    test(problem): check real problem creation

--- tests/integration/problem/domain_creation_test.py
+++ tests/integration/problem/domain_creation_test.py
@@ -2,6 +2,7 @@ import pytest
 from unified_planning.shortcuts import AbstractProblem
 
 from tests.integration.problem.domains import FakeDomain
+from tyr import AbstractDomain, get_all_domains
 
 
 class TestDomainCreation:
@@ -10,7 +11,7 @@ class TestDomainCreation:
         [(str(i).zfill(2), not (0 < i <= 3)) for i in range(15)],
     )
     @pytest.mark.parametrize("version_name", ["a_first", "base", "modified"])
-    def test_problem_creation(
+    def test_fake_domain_problem_creation(
         self,
         problem_id: str,
         is_none: bool,
@@ -22,3 +23,19 @@ class TestDomainCreation:
         assert (version.value is None) == is_none
         if not is_none:
             assert isinstance(version.value, AbstractProblem)
+
+    @pytest.mark.slow
+    @pytest.mark.parametrize("problem_id", [f"{i:0>2}" for i in range(1, 100)])
+    @pytest.mark.parametrize(
+        "domain",
+        get_all_domains(),
+        ids=[d.name for d in get_all_domains()],
+    )
+    def test_real_domain_creation(self, domain: AbstractDomain, problem_id: str):
+        problem = domain.get_problem(problem_id)
+        assert (
+            problem is not None
+        ), f"The domain {domain.name} is empty, please consider to remove it"
+        # Check all versions can successfully be built.
+        for version in problem.versions.values():
+            version.value  # Just check the value can be accessed without error.
