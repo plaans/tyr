9e4d1d016a7fe05647b2379a98f55e6c0085d77d
Roland Godet <r.godet@outlook.fr>
2024-01-14 18:28:44 +0100

    refactor(domains): rename base file folders

--- src/tyr/problem/depots_hierarchical/__init__.py
+++ src/tyr/problem/depots_hierarchical/__init__.py
@@ -10,5 +10,5 @@ from tyr.problem.model import AbstractDomain, ProblemInstance
 
 class DepotsHierarchicalDomain(AbstractDomain):
     def build_problem_base(self, problem: ProblemInstance) -> Optional[AbstractProblem]:
-        folder = Path(__file__).parent / "hddl"
+        folder = Path(__file__).parent / "base"
         return self.load_from_files(folder, problem.uid)
