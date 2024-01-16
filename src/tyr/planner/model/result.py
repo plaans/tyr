9859efb2e42ed37951bc3e8897d3adf63015e7d9
Roland Godet <r.godet@outlook.fr>
2024-01-16 09:52:20 +0100

    feat(planner): add PlannerResult

---
+++ src/tyr/planner/model/result.py
@@ -0,0 +1,88 @@
+from dataclasses import dataclass
+from enum import Enum, auto
+from typing import Optional
+
+from unified_planning.engines.results import (
+    PlanGenerationResult,
+    PlanGenerationResultStatus,
+)
+
+from tyr.problem import ProblemInstance
+
+
+class PlannerResultStatus(Enum):
+    """Represents the possible status of a planner result."""
+
+    SOLVED = auto()
+    UNSOLVABLE = auto()
+    TIMEOUT = auto()
+    MEMOUT = auto()
+    ERROR = auto()
+    UNSUPPORTED = auto()
+
+    @staticmethod
+    def from_upf(status: PlanGenerationResultStatus) -> "PlannerResultStatus":
+        """Converts a status from the unified planning library to our status format.
+
+        Args:
+            status (PlanGenerationResultStatus): The status to convert.
+
+        Returns:
+            PlannerResultStatus: The inner matching status.
+        """
+        return {
+            PlanGenerationResultStatus.SOLVED_OPTIMALLY: PlannerResultStatus.SOLVED,
+            PlanGenerationResultStatus.SOLVED_SATISFICING: PlannerResultStatus.SOLVED,
+            PlanGenerationResultStatus.UNSOLVABLE_PROVEN: PlannerResultStatus.UNSOLVABLE,
+            PlanGenerationResultStatus.UNSOLVABLE_INCOMPLETELY: PlannerResultStatus.UNSOLVABLE,
+            PlanGenerationResultStatus.TIMEOUT: PlannerResultStatus.TIMEOUT,
+            PlanGenerationResultStatus.MEMOUT: PlannerResultStatus.MEMOUT,
+            PlanGenerationResultStatus.INTERNAL_ERROR: PlannerResultStatus.ERROR,
+            PlanGenerationResultStatus.UNSUPPORTED_PROBLEM: PlannerResultStatus.UNSUPPORTED,
+            PlanGenerationResultStatus.INTERMEDIATE: PlannerResultStatus.SOLVED,
+        }[status]
+
+
+@dataclass
+class PlannerResult:
+    """Represents the result of a planner solving a problem."""
+
+    planner_name: str
+    problem: ProblemInstance
+    status: PlannerResultStatus
+    computation_time: Optional[float]
+    plan: Optional[str]
+    plan_quality: Optional[float]
+
+    @staticmethod
+    def from_upf(
+        problem: ProblemInstance,
+        result: PlanGenerationResult,
+    ) -> "PlannerResult":
+        """Converts a result from the unified planning library to our inner result format.
+
+        Args:
+            problem (ProblemInstance): The problem solved by the planner.
+            result (PlanGenerationResult): The result to convert.
+
+        Returns:
+            PlannerResult: The inner matching result.
+        """
+        computation_key = "engine_internal_time"
+        computation_time = None
+        if result.metrics is not None and computation_key in result.metrics:
+            computation_time = float(result.metrics[computation_key])
+
+        plan, plan_quality = None, None
+        if result.plan is not None:
+            plan = str(result.plan)
+            plan_quality = problem.get_quality_of_plan(plan)
+
+        return PlannerResult(
+            result.engine_name,
+            problem,
+            PlannerResultStatus.from_upf(result.status),
+            computation_time,
+            plan,
+            plan_quality,
+        )
---
+++ tests/tyr/planner/model/result_test.py
@@ -0,0 +1,143 @@
+from typing import Any, Dict, Optional
+from unittest.mock import MagicMock, Mock
+
+import pytest
+from unified_planning.engines.results import (
+    PlanGenerationResult,
+    PlanGenerationResultStatus,
+)
+
+from tests.utils import ModelTest
+from tyr import PlannerResult, PlannerResultStatus
+
+
+class TestPlannerResult(ModelTest):
+    # ============================================================================ #
+    #                                    Getters                                   #
+    # ============================================================================ #
+    def get_default_attributes(self) -> Dict[str, Any]:
+        return {
+            "planner_name": "mockplanner",
+            "problem": self._problem(),
+            "status": PlannerResultStatus.SOLVED,
+            "computation_time": None,
+            "plan": None,
+            "plan_quality": None,
+        }
+
+    def get_instance(self) -> PlannerResult:
+        return PlannerResult(
+            planner_name="mockplanner",
+            problem=self._problem(),
+            status=PlannerResultStatus.SOLVED,
+            computation_time=None,
+            plan=None,
+            plan_quality=None,
+        )
+
+    def _problem(self):
+        if not hasattr(self, "_problem_"):
+            self._problem_ = MagicMock()
+        return self._problem_
+
+    @pytest.fixture()
+    def problem(self):
+        yield self._problem()
+
+    @staticmethod
+    @pytest.fixture()
+    def upf_result():
+        yield PlanGenerationResult(
+            PlanGenerationResultStatus.SOLVED_OPTIMALLY,
+            plan=None,
+            engine_name="mockplanner",
+        )
+
+    # ============================================================================ #
+    #                                     Tests                                    #
+    # ============================================================================ #
+
+    # ================================= From UPF ================================= #
+
+    @pytest.mark.parametrize("problem", [MagicMock(), MagicMock()])
+    def test_from_upf_problem(self, problem: Mock, upf_result: PlanGenerationResult):
+        result = PlannerResult.from_upf(problem, upf_result)
+        assert result.problem == problem
+
+    @pytest.mark.parametrize("status", PlanGenerationResultStatus)
+    def test_from_upf_status(
+        self,
+        status: PlanGenerationResultStatus,
+        problem: Mock,
+        upf_result: PlanGenerationResult,
+    ):
+        status_map = {
+            PlanGenerationResultStatus.SOLVED_OPTIMALLY: PlannerResultStatus.SOLVED,
+            PlanGenerationResultStatus.SOLVED_SATISFICING: PlannerResultStatus.SOLVED,
+            PlanGenerationResultStatus.UNSOLVABLE_PROVEN: PlannerResultStatus.UNSOLVABLE,
+            PlanGenerationResultStatus.UNSOLVABLE_INCOMPLETELY: PlannerResultStatus.UNSOLVABLE,
+            PlanGenerationResultStatus.TIMEOUT: PlannerResultStatus.TIMEOUT,
+            PlanGenerationResultStatus.MEMOUT: PlannerResultStatus.MEMOUT,
+            PlanGenerationResultStatus.INTERNAL_ERROR: PlannerResultStatus.ERROR,
+            PlanGenerationResultStatus.UNSUPPORTED_PROBLEM: PlannerResultStatus.UNSUPPORTED,
+            PlanGenerationResultStatus.INTERMEDIATE: PlannerResultStatus.SOLVED,
+        }
+        upf_result.status = status
+        result = PlannerResult.from_upf(problem, upf_result)
+        assert result.status == status_map[status]
+
+    @pytest.mark.parametrize("name", ["mockplanner", "mockplannerbis"])
+    def test_from_upf_planner_name(
+        self, name: str, problem: Mock, upf_result: PlanGenerationResult
+    ):
+        upf_result.engine_name = name
+        result = PlannerResult.from_upf(problem, upf_result)
+        assert result.planner_name == name
+
+    @pytest.mark.parametrize("computation_time", [None, "1.5", "0.0", "15"])
+    def test_from_upf_computation_time(
+        self,
+        computation_time: Optional[str],
+        problem: Mock,
+        upf_result: PlanGenerationResult,
+    ):
+        if computation_time is None:
+            upf_result.metrics = None
+            expected = None
+        else:
+            upf_result.metrics = {"engine_internal_time": computation_time}
+            expected = float(computation_time)
+        result = PlannerResult.from_upf(problem, upf_result)
+        assert result.computation_time == expected
+
+    @pytest.mark.parametrize("plan", [None, MagicMock(), MagicMock()])
+    def test_from_upf_plan(
+        self,
+        plan: Optional[Mock],
+        problem: Mock,
+        upf_result: PlanGenerationResult,
+    ):
+        upf_result.plan = plan
+        expected = None if plan is None else str(plan)
+        result = PlannerResult.from_upf(problem, upf_result)
+        assert result.plan == expected
+
+    @pytest.mark.parametrize(
+        ["plan", "quality"], [(None, None), (MagicMock(), 15), (MagicMock(), 7.6)]
+    )
+    def test_from_upf_plan_quality(
+        self,
+        plan: Optional[Mock],
+        quality: Optional[float],
+        problem: Mock,
+        upf_result: PlanGenerationResult,
+    ):
+        upf_result.plan = plan
+        problem.get_quality_of_plan.return_value = quality
+        result = PlannerResult.from_upf(problem, upf_result)
+
+        assert result.plan_quality == quality
+        if plan is None:
+            problem.get_quality_of_plan.assert_not_called()
+        else:
+            problem.get_quality_of_plan.assert_called_once_with(str(plan))
