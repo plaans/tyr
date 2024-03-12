e72597ceb74e2d4caa4fd37049672c3465bc543b
Roland Godet <r.godet@outlook.fr>
2024-03-12 14:03:44 +0100

    feat(config): can specify the full path of the configuration file to the loader

--- src/tyr/configuration/loader.py
+++ src/tyr/configuration/loader.py
@@ -1,10 +1,10 @@
 from pathlib import Path
-from typing import Any
+from typing import Any, Optional
 
 import yaml
 
 
-def load_config(name: str) -> Any:
+def load_config(name: str, path: Optional[Path] = None) -> Any:
     """
     Loads a configuration file from the `tyr.configuration` module.
 
@@ -13,15 +13,21 @@ def load_config(name: str) -> Any:
 
     Args:
         name (str): Name of the configuration file.
+        path (Optional[Path], optional): Path to the file. Defaults to None.
+            If provided, it will load the configuration from this file and ignore `name` parameter.
 
     Returns:
         Any: The content of the file.
     """
     import tyr.configuration as config_module  # pylint: disable=import-outside-toplevel
 
-    config_file = (Path(config_module.__path__[0]) / f"{name}.yaml").resolve()
-    if not config_file.exists():
-        config_file = config_file.parent / f"{name}.example.yaml"
+    if path is not None:
+        config_file = path
+    else:
+        config_file = (Path(config_module.__path__[0]) / f"{name}.yaml").resolve()
+        if not config_file.exists():
+            config_file = config_file.parent / f"{name}.example.yaml"
+
     with open(config_file, "r", encoding="utf-8") as file:
         content = yaml.safe_load(file)
 
--- tests/tyr/configuration/loader_test.py
+++ tests/tyr/configuration/loader_test.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import pytest
 import tests.tyr.planners.fixtures.configuration as config_module
-from unittest.mock import Mock, patch
+from unittest.mock import MagicMock, Mock, patch
 
 from tyr.configuration.loader import load_config
 
@@ -13,7 +13,7 @@ class TestLoader:
     @patch("pathlib.Path.exists")
     @patch("yaml.safe_load")
     @pytest.mark.parametrize("name", ["planners", "domains"])
-    def test_get_all_planner_configs_loads_local_file_if_present(
+    def test_load_config_loads_local_file_if_present(
         self, mock_yaml, mock_path, mock_module, mock_open, name
     ):
         mock_module.__path__ = config_module.__path__
@@ -30,7 +30,7 @@ class TestLoader:
     @patch("pathlib.Path.exists")
     @patch("yaml.safe_load")
     @pytest.mark.parametrize("name", ["planners", "domains"])
-    def test_get_all_planner_configs_loads_example_file_if_local_is_absent(
+    def test_load_config_loads_example_file_if_local_is_absent(
         self, mock_yaml, mock_path, mock_module, mock_open, name
     ):
         mock_module.__path__ = config_module.__path__
@@ -39,5 +39,15 @@ class TestLoader:
         mock_yaml.return_value = Mock()
         file = (Path(config_module.__path__[0]) / f"{name}.example.yaml").resolve()
         result = load_config(name)
+        mock_open.assert_called_once_with(file, "r", encoding="utf-8")
+        assert result == mock_yaml.return_value
+
+    @patch("builtins.open")
+    @patch("yaml.safe_load")
+    def test_load_config_loads_specific_file_if_provided(self, mock_yaml, mock_open):
+        mock_open.return_value.__enter__.return_value.read.return_value = ""
+        mock_yaml.return_value = Mock()
+        file = MagicMock()
+        result = load_config("", file)
         mock_open.assert_called_once_with(file, "r", encoding="utf-8")
         assert result == mock_yaml.return_value
