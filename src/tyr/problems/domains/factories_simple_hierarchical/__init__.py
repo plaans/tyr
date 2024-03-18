d7a446ead71890c992ae8776aa4c4b1d7b11e4ea
Roland Godet <r.godet@outlook.fr>
2024-03-18 08:35:34 +0100

    feat(domains): add Factories Simple

--- src/tyr/problems/domains/factories_simple_hierarchical/__init__.py
+++ src/tyr/problems/domains/factories_simple_hierarchical/__init__.py
@@ -1,48 +1,20 @@
 from pathlib import Path
-from typing import Optional, Union
+from typing import Optional
 
 from unified_planning.io.pddl_reader import PDDLReader
-from unified_planning.model.htn import HierarchicalProblem
 from unified_planning.shortcuts import AbstractProblem
 
 from tyr.problems.model import AbstractDomain, ProblemInstance
 
+FOLDER = Path(__file__).parent / "base"
 
-class SimpleGotoHierarchicalDomain(AbstractDomain):
+
+class FactoriesSimpleHierarchicalDomain(AbstractDomain):
     def get_num_problems(self) -> int:
-        return 30
+        return len([f for f in FOLDER.iterdir() if "instance" in f.name])
 
     def build_problem_base(self, problem: ProblemInstance) -> Optional[AbstractProblem]:
-        domain_file = Path(__file__).parent / "base" / "domain.hddl"
-        pb: HierarchicalProblem = PDDLReader().parse_problem(domain_file.as_posix())
-
-        roads = [(1, 2), (1, 3), (1, 4), (2, 3), (2, 4), (3, 5), (4, 5)]
-        positions = set().union(*roads)
-
-        # Types
-        position_type = pb.user_type("position")
-        truck_type = pb.user_type("truck")
-
-        # Objects
-        for position in positions:
-            pb.add_object(f"P{position}", position_type)
-        pb.add_object("T1", truck_type)
-
-        # Initial values
-        for road in roads:
-            source = pb.object(f"P{road[0]}")
-            target = pb.object(f"P{road[1]}")
-            pb.set_initial_value(pb.fluent("road")(source, target), True)
-            pb.set_initial_value(pb.fluent("road")(target, source), True)
-        pb.set_initial_value(pb.fluent("at")(pb.object("T1"), pb.object("P1")), True)
-
-        # Goals
-        for _ in range(int(problem.uid) * 10):
-            pb.task_network.add_subtask(
-                pb.get_task("goto"), pb.object("T1"), pb.object("P5")
-            )
-
-        return pb
+        return self.load_from_files(FOLDER, problem.uid)
 
     def build_problem_exponential(
         self, problem: ProblemInstance
@@ -53,13 +25,13 @@ class SimpleGotoHierarchicalDomain(AbstractDomain):
         self, problem: ProblemInstance
     ) -> Optional[AbstractProblem]:
         # Get the base version of the problem.
-        base: HierarchicalProblem = problem.versions["base"].value
+        base = problem.versions["exponential"].value
         if base is None:
             return None
 
         # Create the new domain.
         domain_file = Path(__file__).parent / "linear" / "domain.hddl"
-        pb: HierarchicalProblem = PDDLReader().parse_problem(domain_file.as_posix())
+        pb = PDDLReader().parse_problem(domain_file.as_posix())
 
         # Add all objects.
         pb.add_objects(base.all_objects)
@@ -78,13 +50,13 @@ class SimpleGotoHierarchicalDomain(AbstractDomain):
         self, problem: ProblemInstance
     ) -> Optional[AbstractProblem]:
         # Get the base version of the problem.
-        base: HierarchicalProblem = problem.versions["base"].value
+        base = problem.versions["exponential"].value
         if base is None:
             return None
 
         # Create the new domain.
         domain_file = Path(__file__).parent / "insertion" / "domain.hddl"
-        pb: HierarchicalProblem = PDDLReader().parse_problem(domain_file.as_posix())
+        pb = PDDLReader().parse_problem(domain_file.as_posix())
 
         # Add all objects.
         pb.add_objects(base.all_objects)
@@ -97,11 +69,11 @@ class SimpleGotoHierarchicalDomain(AbstractDomain):
         for task in base.task_network.subtasks:
             # Convert the goto task into a at goal.
             if task.task.name == "goto":
-                pb.add_goal(pb.fluent("at")(task.parameters[0], task.parameters[1]))
+                pb.add_goal(pb.fluent("truck-at")(task.parameters[0]))
             else:
                 pb.task_network.add_subtask(task)
 
-        # Add the free-move task for task insertion.
-        pb.task_network.add_subtask(pb.get_task("free-move"), pb.object("T1"))
+        # Add the free-drive task for task insertion.
+        pb.task_network.add_subtask(pb.get_task("free-move"))
 
         return pb
