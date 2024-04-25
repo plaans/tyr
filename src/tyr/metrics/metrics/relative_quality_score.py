5f970c856da9a00a57848b3b380e9b4631169986
Roland Godet <r.godet@outlook.fr>
2024-04-25 13:28:17 +0200

    feat(metrics): add relative quality score

---
+++ src/tyr/metrics/metrics/relative_quality_score.py
@@ -0,0 +1,22 @@
+from typing import List
+from tyr.metrics.metric import Metric
+from tyr.metrics.metrics.coverage import CoverageMetric
+from tyr.metrics.metrics.quality_score import QualityScoreMetric
+from tyr.planners.model.result import PlannerResult
+
+
+class RelativeQualityScoreMetric(Metric):
+    """A metric to evaluate the quality score of a planner relatively to its coverage."""
+
+    def abbrev(self) -> str:
+        return "RS"
+
+    # pylint: disable=protected-access
+    def _evaluate(
+        self, results: List[PlannerResult], all_results: List[PlannerResult]
+    ) -> float:
+        cov = CoverageMetric()._evaluate(results, all_results)
+        if cov == 0:
+            return 0
+        qs = QualityScoreMetric()._evaluate(results, all_results)
+        return qs / cov * 100
---
+++ tests/tyr/metrics/metrics/relative_quality_score_test.py
@@ -0,0 +1,37 @@
+from unittest.mock import MagicMock
+
+import pytest
+
+from tyr.metrics.metrics.coverage import CoverageMetric
+from tyr.metrics.metrics.quality_score import QualityScoreMetric
+from tyr.metrics.metrics.relative_quality_score import RelativeQualityScoreMetric
+from tyr.planners.model.result import PlannerResult
+
+
+class TestRelativeQualityScoreMetric:
+    def test_abbrev(self):
+        metric = RelativeQualityScoreMetric()
+        assert metric.abbrev() == "RS"
+
+    @pytest.mark.parametrize(
+        "cov, qs, expected",
+        [(0, 50, 0), (30, 30, 100), (80, 40, 50)],
+    )
+    def test_evaluate(self, cov, qs, expected):
+        results = [MagicMock(spec=PlannerResult), MagicMock(spec=PlannerResult)]
+        all_results = [MagicMock(spec=PlannerResult), MagicMock(spec=PlannerResult)]
+
+        coverage_metric = CoverageMetric()
+        quality_score_metric = QualityScoreMetric()
+
+        coverage_metric._evaluate = MagicMock(return_value=cov)
+        quality_score_metric._evaluate = MagicMock(return_value=qs)
+
+        metric = RelativeQualityScoreMetric()
+        assert metric._evaluate(results, all_results) == expected
+
+        coverage_metric._evaluate.assert_called_once_with(results, all_results)
+        if cov != 0:
+            quality_score_metric._evaluate.assert_called_once_with(results, all_results)
+        else:
+            quality_score_metric._evaluate.assert_not_called()
