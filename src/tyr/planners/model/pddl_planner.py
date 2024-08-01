2bb8b7e5582b44a87b4ee4d820c2f581aff6f0f2
Roland Godet <r.godet@outlook.fr>
2024-08-01 11:16:25 +0200

    fix(planners): catch errors and handle writer as output_stream for PDDL planners

--- src/tyr/planners/model/pddl_planner.py
+++ src/tyr/planners/model/pddl_planner.py
@@ -3,7 +3,7 @@ import re
 import time
 from typing import IO, Callable, Dict, List, Optional, Tuple, Union
 
-from unified_planning.engines.pddl_anytime_planner import PDDLAnytimePlanner
+from unified_planning.engines.pddl_anytime_planner import PDDLAnytimePlanner, Writer
 from unified_planning.engines.pddl_planner import run_command_posix_select
 from unified_planning.engines.results import (
     LogLevel,
@@ -55,89 +55,102 @@ class TyrPDDLPlanner(PDDLAnytimePlanner):
         output_stream: Optional[Union[Tuple[IO[str], IO[str]], IO[str]]] = None,
         anytime: bool = False,
     ) -> PlanGenerationResult:
-        self._writer = TyrPDDLWriter(
-            problem,
-            self._needs_requirements,
-            self._rewrite_bool_assignments,
-        )
-        plan = None
-        logs: List[LogMessage] = []
-
-        if output_stream is None:
-            raise RuntimeError("Output stream is required for Tyr PDDL planners.")
-        if isinstance(output_stream, tuple):
-            output_stream = output_stream[0]
-        output_dir = os.sep.join(output_stream.name.split(os.sep)[:-1])
-
-        ext = self._file_extension()
-        domain_filename = os.path.join(output_dir, f"domain.{ext}")
-        problem_filename = os.path.join(output_dir, f"problem.{ext}")
-        plan_filename = os.path.join(output_dir, "output.plan")
-        domain_options = self._get_write_domain_options()
-        self._writer.write_domain(domain_filename, **domain_options)
-        self._writer.write_problem(problem_filename)
-        if anytime:
-            cmd = self._get_anytime_cmd(
-                domain_filename,
-                problem_filename,
-                plan_filename,
-            )
-        else:
-            cmd = self._get_cmd(
-                domain_filename,
-                problem_filename,
-                plan_filename,
-            )
-        process_start = time.time()
-
-        exec_res = run_command_posix_select(self, cmd, output_stream, timeout)
-        timeout_occurred, (proc_out, proc_err), retval = exec_res
-
-        process_end = time.time()
-        logs.append(LogMessage(LogLevel.INFO, "".join(proc_out)))
-        logs.append(LogMessage(LogLevel.ERROR, "".join(proc_err)))
-        if os.path.isfile(plan_filename):
-            plan = self._plan_from_file(
+        try:
+            self._writer = TyrPDDLWriter(
                 problem,
-                plan_filename,
-                self._writer.get_item_named,
+                self._needs_requirements,
+                self._rewrite_bool_assignments,
             )
-        else:
-            plan = self._plan_from_str(
-                problem,
-                self._get_plan(proc_out),
-                self._writer.get_item_named,
-            )
+            plan = None
+            logs: List[LogMessage] = []
 
-        metrics = {}
-        if (computation := self._get_computation_time(logs)) is not None:
-            metrics["engine_internal_time"] = str(computation)
-        else:
-            metrics["engine_internal_time"] = str(process_end - process_start)
-        if timeout_occurred and retval != 0:
-            return PlanGenerationResult(
-                PlanGenerationResultStatus.TIMEOUT,
-                plan=plan,
+            if output_stream is None:
+                raise RuntimeError("Output stream is required for Tyr PDDL planners.")
+            if isinstance(output_stream, tuple):
+                output_stream = output_stream[0]
+            base_stream = (
+                output_stream._output_stream  # pylint: disable=protected-access
+                if isinstance(output_stream, Writer)
+                else output_stream
+            )
+            output_dir = os.sep.join(base_stream.name.split(os.sep)[:-1])
+
+            ext = self._file_extension()
+            domain_filename = os.path.join(output_dir, f"domain.{ext}")
+            problem_filename = os.path.join(output_dir, f"problem.{ext}")
+            plan_filename = os.path.join(output_dir, "output.plan")
+            domain_options = self._get_write_domain_options()
+            self._writer.write_domain(domain_filename, **domain_options)
+            self._writer.write_problem(problem_filename)
+            if anytime:
+                cmd = self._get_anytime_cmd(
+                    domain_filename,
+                    problem_filename,
+                    plan_filename,
+                )
+            else:
+                cmd = self._get_cmd(
+                    domain_filename,
+                    problem_filename,
+                    plan_filename,
+                )
+            process_start = time.time()
+
+            exec_res = run_command_posix_select(self, cmd, output_stream, timeout)
+            timeout_occurred, (proc_out, proc_err), retval = exec_res
+
+            process_end = time.time()
+            logs.append(LogMessage(LogLevel.INFO, "".join(proc_out)))
+            logs.append(LogMessage(LogLevel.ERROR, "".join(proc_err)))
+            if os.path.isfile(plan_filename):
+                plan = self._plan_from_file(
+                    problem,
+                    plan_filename,
+                    self._writer.get_item_named,
+                )
+            else:
+                plan = self._plan_from_str(
+                    problem,
+                    self._get_plan(proc_out),
+                    self._writer.get_item_named,
+                )
+
+            metrics = {}
+            if (computation := self._get_computation_time(logs)) is not None:
+                metrics["engine_internal_time"] = str(computation)
+            else:
+                metrics["engine_internal_time"] = str(process_end - process_start)
+            if timeout_occurred and retval != 0:
+                return PlanGenerationResult(
+                    PlanGenerationResultStatus.TIMEOUT,
+                    plan=plan,
+                    engine_name=self.name,
+                    log_messages=logs,
+                    metrics=metrics,
+                )
+
+            status = self._result_status(problem, plan, retval, logs)
+            res = PlanGenerationResult(
+                status,
+                plan,
                 engine_name=self.name,
                 log_messages=logs,
                 metrics=metrics,
             )
-
-        status = self._result_status(problem, plan, retval, logs)
-        res = PlanGenerationResult(
-            status,
-            plan,
-            engine_name=self.name,
-            log_messages=logs,
-            metrics=metrics,
-        )
-        problem_kind = problem.kind
-        if problem_kind.has_continuous_time() or problem_kind.has_discrete_time():
-            if isinstance(plan, TimeTriggeredPlan) or plan is None:
-                return correct_plan_generation_result(
-                    res, problem, self._get_engine_epsilon()
-                )
-        return res
+            problem_kind = problem.kind
+            if problem_kind.has_continuous_time() or problem_kind.has_discrete_time():
+                if isinstance(plan, TimeTriggeredPlan) or plan is None:
+                    return correct_plan_generation_result(
+                        res, problem, self._get_engine_epsilon()
+                    )
+            return res
+        except Exception as e:  # pylint: disable=broad-exception-caught
+            return PlanGenerationResult(
+                PlanGenerationResultStatus.INTERNAL_ERROR,
+                plan=None,
+                engine_name=self.name,
+                log_messages=[LogMessage(LogLevel.ERROR, str(e))],
+            )
 
 
 __all__ = ["TyrPDDLPlanner"]
