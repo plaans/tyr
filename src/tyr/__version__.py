75862adec1e115c6bcea5248d5597e383a31d192
Roland Godet <r.godet@outlook.fr>
2024-08-02 11:58:58 +0200

    style: linters

--- src/tyr/__version__.py
+++ src/tyr/__version__.py
@@ -5,14 +5,20 @@ import toml
 
 
 def load_version():
+    """
+    Load the version from the pyproject.toml file if it exists,
+    otherwise use importlib.metadata.version
+    """
     pyproject_toml_file = Path(__file__).parent.parent.parent / "pyproject.toml"
     if pyproject_toml_file.exists() and pyproject_toml_file.is_file():
         pyproject_toml = toml.load(pyproject_toml_file)
         name = pyproject_toml["project"]["name"]
-    try:
-        return importlib.metadata.version(name)
-    except importlib.metadata.PackageNotFoundError:
-        return toml.load(pyproject_toml_file)["project"]["version"] + "-dev"
+        try:
+            return importlib.metadata.version(name)
+        except importlib.metadata.PackageNotFoundError:
+            return toml.load(pyproject_toml_file)["project"]["version"] + "-dev"
+    else:
+        return importlib.metadata.version("tyr")
 
 
 __version__ = load_version()
